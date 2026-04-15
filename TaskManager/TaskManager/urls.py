019b551082fcd66ec5520aa08ec87f83c9235fb7
minzawhtay-umgodoodev18 <umgodoodev18@gmail.com>
2026-04-15 21:02:58 +0630

    [IMP]:upload github

--- TaskManager/TaskManager/urls.py
+++ TaskManager/TaskManager/urls.py
@@ -19,13 +19,26 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Tasks.urls')),
 
     path('login/', LoginView.as_view(template_name='Tasks/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    
+    # ✅ GOOGLE VERIFICATION (ADD HERE)
+    path(
+        'google1234567890abcdef.html',
+        TemplateView.as_view(
+            template_name='google1234567890abcdef.html',
+            content_type='text/html'
+        )
+    ),
 ]
+]
+
 # ✅ MEDIA FILES (ONLY IN DEVELOPMENT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
